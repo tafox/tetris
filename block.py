2bd36b2b0eb82d4c1198d12c59a0567ab04efc88
Thomas Fox <thomas.allan.fox@gmail.com>
2015-01-11 12:08:57 -0500

    Added DeadBlocks class.

--- block.py
+++ block.py
@@ -20,15 +20,24 @@ class Block:
     if shape == "I": 
       self.center = [position[0]+30,position[1]+7.5]
       self.vertices = [position,
+                      [position[0]+15,position[1]],
+                      [position[0]+30,position[1]],
+                      [position[0]+45,position[1]],
                       [position[0]+60,position[1]],
                       [position[0]+60,position[1]+15],
-                      [position[0], position[1]+15]]
+                      [position[0]+45,position[1]+15],
+                      [position[0]+30,position[1]+15],
+                      [position[0]+15,position[1]+15],
+                      [position[0],position[1]+15]]
       self.color = [69,226,243]
     if shape == "J":
       self.center = [position[0]+22.5,position[1]+15]
       self.vertices = [position,
+                      [position[0]+15,position[1]],
+                      [position[0]+30,position[1]],
                       [position[0]+45,position[1]],
                       [position[0]+45,position[1]+15],
+                      [position[0]+30,position[1]+15],
                       [position[0]+15,position[1]+15],
                       [position[0]+15,position[1]+30],
                       [position[0],position[1]+30]]
@@ -36,18 +45,26 @@ class Block:
     if shape == "L":
       self.center = [position[0]+22.5,position[1]+15]
       self.vertices = [position,
+                      [position[0]+15,position[1]],
+                      [position[0]+30,position[1]],
                       [position[0]+45,position[1]],
-                      [position[0]+45,position[1]+30],
-                      [position[0]+30,position[1]+30],
+                      [position[0]+45,position[1]+15],
                       [position[0]+30,position[1]+15],
+                      [position[0]+15,position[1]+15],
+                      [position[0]+15,position[1]+30],
+                      [position[0],position[1]+30],
                       [position[0],position[1]+15]]
       self.color = [238,152,5]
     if shape == "O":
       self.center = [position[0]+15,position[1]+15]
       self.vertices = [position,
+                      [position[0]+15,position[1]],
                       [position[0]+30,position[1]],
+                      [position[0]+30,position[1]+15],
                       [position[0]+30,position[1]+30],
-                      [position[0],position[1]+30]]
+                      [position[0]+15,position[1]+30],
+                      [position[0],position[1]+30],
+                      [position[0],position[1]+15]]
       self.color = [230,255,0]
     if shape == "S":
       self.center = [position[0]+0,position[1]+15]
@@ -85,4 +102,7 @@ class Block:
       y = vertex[1] - self.center[1]
       vertex[0] = (x*math.cos(math.pi/2)+y*math.sin(math.pi/2)) + self.center[0]
       vertex[1] = (x*-math.sin(math.pi/2)+y*math.cos(math.pi/2)) + self.center[1]
-    
+
+class DeadBlocks:
+  def __init__(self):
+    self.blocks = []   
